27acd9dbc9823b74ec1b219154e0c5f4eaedf8cb
James Santos <jamesrrs.me@gmail.com>
2023-12-02 18:38:12 -0800

    update

--- src/app.js
+++ src/app.js
@@ -6,9 +6,13 @@ const dotenv = require("dotenv").config();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const path = require("path"); // Require the path module
+const logger = require("./config/winston");
 
 // Create an Express application
 const app = express();
+
+// Rate limiting
+// const rateLimit = require("./middleware/rateLimit");
 
 // Use various middlewares
 app.use(helmet());
@@ -17,6 +21,26 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+// Apply rate limiting
+// app.use(rateLimit);
+
+app.use("/status", (req, res) => {
+  res.send("OK");
+}); // health check endpoint
+
+app.use((err, req, res, next) => {
+  if (err.status === 429) {
+    return res.status(429).json({
+      message: "Too many requests, please try again later",
+    });
+  }
+
+  logger.error(err.stack);
+
+  res.status(500).json({
+    message: "Internal server error",
+  });
+});
 
 // Serve Swagger UI static files
 app.use(
@@ -34,12 +58,6 @@ app.use("/archetypes", archetypesRoutes);
 // Handle 404 errors
 app.use((req, res, next) => {
   res.status(404).json({ error: "Not found" });
-});
-
-// Error handler
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).json({ error: "Internal server error" });
 });
 
 // Start the server
