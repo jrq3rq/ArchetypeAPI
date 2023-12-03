f8dbcebf2042d56118d49647dbd9e56d8755fd80
James Santos <jamesrrs.me@gmail.com>
2023-12-02 18:38:12 -0800

    update

--- src/middlewares/rateLimit.js
+++ src/middlewares/rateLimit.js
@@ -1,8 +1,34 @@
+// src/middleware/rateLimit.js
+
 const rateLimit = require("express-rate-limit");
+const logger = require("../config/winston");
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: "Too many requests, please try again later",
 });
 
-module.exports = limiter;
+module.exports = (req, res, next) => {
+  // Allow healthchecks to pass through
+  if (req.path === "/status") {
+    return next();
+  }
+
+  limiter(req, res, (err) => {
+    if (err) {
+      // Explicitly send 429 when rate limit exceeded
+      return res.status(429).json({
+        status: "error",
+        message: err.message,
+      });
+    }
+
+    next();
+  });
+};
+
+// Log when rate limit exceeded
+limiter.on("rateLimitExceeded", (req, res) => {
+  logger.warn(`Rate limit exceeded for client IP ${req.ip}`);
+});
